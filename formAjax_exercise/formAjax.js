54a5b90c3eb11e7cc6aab96b9e6bab5686eb0d7c
ThangTruong <tuyenamai@gmail.com>
2018-08-15 17:20:40 +0700

    complete form ajax layout

--- formAjax_exercise/formAjax.js
+++ formAjax_exercise/formAjax.js
@@ -1,3 +1,16 @@
 window.onload = function() {
+    changeSelectElement(month, year);
+    //Get a day 
+    var first_date = month_name[month] + " " + 1 + " " + year; // August 1 2018
+    var temp = new Date(first_date).toString(); // Wed, Aug 1 2018
+    var first_day = temp.substring(0, 3); // return Wed
+
+    var day_num = day_name.indexOf(first_day); // return 3
+    //Get the last day
+    var days = new Date(year, month + 1, 0).getDate(); //31
+    var calendar = get_calendar(day_num, days, d.getDate());
+    // document.getElementById("calendar-month-year").innerHTML = month_name[month] + " " + year;
+    document.getElementById("calendar-dates").appendChild(calendar);
+    document.getElementById("myId").style.visibility = "hidden";
     document.getElementById("calendar-tools").style.visibility = "hidden";
 }
